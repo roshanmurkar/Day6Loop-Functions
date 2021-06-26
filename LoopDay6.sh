f5fd30c2bc205c5cfadfe425fcf8c8d03b0b2c44
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-06-26 18:48:15 +0530

    is prime

--- LoopDay6.sh
+++ LoopDay6.sh
@@ -1,2 +1,17 @@
 #!/bin/bash -x
 
+read -p "Enter a number : " n
+base=0;
+a=0;
+
+b=n/2;
+
+for (( counter=2; counter<=b; counter++ ))
+do
+        if [ $(($n%$counter)) -eq 0 ]
+        then
+                echo "$n is not prime"
+        else
+                echo "$n is prime"
+        fi
+done
