b7c04bc226a522fb19974c644a7442f0801b7279
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-06-27 14:58:35 +0530

    is palindrome or not

--- FunctionDay6.sh
+++ FunctionDay6.sh
@@ -1,2 +1,24 @@
 #!/bin/bash -x
 
+read -p "Enter the number" n
+
+function palindrome
+{
+	number=$n
+	reverse=0
+	while [ $n -gt 0 ]
+	do
+	a=`expr $n % 10 `
+	n=`expr $n / 10 `
+	reverse=`expr $reverse \* 10 + $a`
+	done
+	echo $reverse
+		if [ $number -eq $reverse ]
+		then
+		    echo "Number is palindrome"
+		else
+		    echo "Number is not palindrome"
+		fi
+}
+result =`palindrome $n`
+echo $result
