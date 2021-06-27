98dd9fda57b89b9ff82c4425b6ae4963d325bda1
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-06-27 14:54:24 +0530

    Bets

--- WhileLoopDay6.sh
+++ WhileLoopDay6.sh
@@ -1,2 +1,32 @@
 #!/bin/bash -x
 
+StartingValue=100
+CountNumofBet=0
+CountNumofWin=0
+
+while [ $StartingValue -lt 200 ] && [ $StartingValue -gt 0 ]
+do
+	bet=$((RANDOM%2))
+
+	if [ $bet -eq 1 ]
+	then
+		((StartingValue++))
+		((CountNumofWin++))
+
+	else
+		(( StartingValue-- ))
+
+	fi
+
+		(( CountNumofBet++ ))
+done
+
+if [ $StartingValue -eq 200 ]
+then
+	echo "Gambler wins and reached limit of $Starting Value "
+	echo "Total bets made $CountNumofBet"
+else
+	echo "Gambler losses and reached limit of $Starting Value "
+	echo "Total bets made $CountNumofBet"
+
+fi
