09d2c1c5812036824425d33c2995d4a1573a0cb9
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-06-27 14:50:29 +0530

    dice

--- WhileLoopDay6.sh
+++ WhileLoopDay6.sh
@@ -1,2 +1,32 @@
 #!/bin/bash -x
 
+read -p "Please enter to roll the dice "
+Heads=0
+Tails=0
+count=1
+while [[ $count != 21 ]]
+do
+	(( count++ ))
+	x=$((RANDOM%10))
+
+	flip=$(( x % 2 ))
+
+	if [ $flip -eq 0 ]
+
+	then
+		((Heads++))
+	else
+		((Tails++))
+	fi
+
+done
+
+echo "Number of Heads" $Heads
+echo "Number of Tails" $Tails
+
+if [ $Heads -gt $Tails ]
+then
+	echo "Heads Win"
+else
+	echo "Tails Win"
+fi
